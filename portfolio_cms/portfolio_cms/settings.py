363405d9b6d38e3f36d7484847831d1b08948490
Eminenz07 <emmyadeoluwa@gmail.com>
2026-01-03 01:56:49 +0100

    Remove hardcoded secrets from settings

--- portfolio_cms/portfolio_cms/settings.py
+++ portfolio_cms/portfolio_cms/settings.py
@@ -143,9 +143,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 # Cloudinary Configuration
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME', 'dg28aohu0'),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY', '424367429367357'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET', 'hEpfvh009SQGZDa1uJJw0L879-8'),
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME', ''),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY', ''),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET', ''),
 }
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
