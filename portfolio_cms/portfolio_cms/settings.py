18bef2903e2ea262650bf7416437fbea70554643
Eminenz07 <emmyadeoluwa@gmail.com>
2026-01-03 01:35:58 +0100

    Set DEBUG=False for production

--- portfolio_cms/portfolio_cms/settings.py
+++ portfolio_cms/portfolio_cms/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-test-key-replace-in-prod')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
